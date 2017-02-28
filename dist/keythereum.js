c9d0e48fe166fc2de71a21d5fea63ad8c55a79d3
Jack Peterson <jack@tinybike.net>
2017-02-28 12:04:21 -0800

    Removed outer try/catch from asynchronous scrypt key derivation

--- dist/keythereum.js
+++ dist/keythereum.js
@@ -193,18 +193,12 @@ module.exports = {
       options.kdfparams = options.kdfparams || {};
 
       // convert strings to buffers
-      if (password.constructor === String)
-        password = str2buf(password, "utf8");
-      if (salt.constructor === String)
-        salt = str2buf(salt);
+      if (password.constructor === String) password = str2buf(password, "utf8");
+      if (salt.constructor === String) salt = str2buf(salt);
 
       // use scrypt as key derivation function
       if (options.kdf === "scrypt") {
-
-        try {
-
-          if (isFunction(cb)) {
-
+        if (isFunction(cb)) {
             setTimeout(function () {
               cb(new Buffer(
                 scrypt.to_hex(scrypt.crypto_scrypt(
@@ -217,38 +211,32 @@ module.exports = {
                 )
               ), "hex"));
             }, 0);
-
           } else {
-
-            return new Buffer(
-              scrypt.to_hex(scrypt.crypto_scrypt(
-                password,
-                salt,
-                options.kdfparams.n || this.constants.scrypt.n,
-                options.kdfparams.r || this.constants.scrypt.r,
-                options.kdfparams.p || this.constants.scrypt.p,
-                options.kdfparams.dklen || this.constants.scrypt.dklen
-              )
-            ), "hex");
-
+            try {
+              return new Buffer(
+                scrypt.to_hex(scrypt.crypto_scrypt(
+                  password,
+                  salt,
+                  options.kdfparams.n || this.constants.scrypt.n,
+                  options.kdfparams.r || this.constants.scrypt.r,
+                  options.kdfparams.p || this.constants.scrypt.p,
+                  options.kdfparams.dklen || this.constants.scrypt.dklen
+                )
+              ), "hex");
+            } catch (ex) {
+              if (isFunction(cb)) {
+                cb(ex);
+              } else {
+                return ex;
+              }
+            }
           }
-
-        } catch (ex) {
-          if (isFunction(cb)) {
-            cb(ex);
-          } else {
-            return ex;
-          }
-        }
 
       // use default key derivation function (PBKDF2)
       } else {
-
         var prf = options.kdfparams.prf || this.constants.pbkdf2.prf;
         if (prf === "hmac-sha256") prf = "sha256";
-
         if (isFunction(cb)) {
-
           if (!this.crypto.pbkdf2) {
             setTimeout(function () {
               cb(new Buffer(sjcl.codec.hex.fromBits(sjcl.misc.pbkdf2(
@@ -271,11 +259,8 @@ module.exports = {
               }
             );
           }
-
         } else {
-
           try {
-
             if (!this.crypto.pbkdf2Sync) {
               return new Buffer(sjcl.codec.hex.fromBits(sjcl.misc.pbkdf2(
                 password.toString('utf8'),
@@ -292,7 +277,6 @@ module.exports = {
                 prf
               );
             }
-
           } catch (ex) {
             return ex;
           }
